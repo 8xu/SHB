84911d6808cf81991e63c38f48ae4f9eee70ee5e
Dragomir Mihai <mischudrag@gmail.com>
2022-01-07 21:44:27 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SHB
+> 🪶 Lightweight Steam Hour Booster
+
+
+
+## How to run
+
+1. Install [node](https://nodejs.org/en/) on your machine.
+2. Run `npm install .` in the directory to install dependencies.
+3. Modify the [config](./config.json) file to your desired configuration.
+    > Get steam game IDs [here](https://steamdb.info/apps/).
+4. Run `node main.js` to start the hour booster.
---
+++ main.js
@@ -0,0 +1,18 @@
+const Steam = require('steam-user');
+const config = require('./config.json'); // Imports required information from config.json
+const client = new Steam();
+
+client.logOn({
+    "accountName": config.username,
+    "password": config.password
+}); // Logs in to Steam with username and password
+
+client.on("loggedOn", () => {
+    console.log(`Logged in as ${config.username}.`);
+	client.setPersona(Steam.EPersonaState.Online);
+	client.gamesPlayed(config.games);
+});
+
+client.on('error', (error) => {
+    console.log(error);
+}); // For error catching
